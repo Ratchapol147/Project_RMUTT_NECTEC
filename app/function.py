95c889d271026bac4efc67da13973359a23bf248
Ratchapol <ohm147_@hotmail.com>
2023-03-05 19:00:56 +0700

    func & views

--- app/function.py
+++ app/function.py
@@ -1,4 +1,5 @@
 from pythainlp.tokenize import word_tokenize
+import requests
 def checkword(con,txt):
      if con.find(txt) > -1: # find indexOf
          return True
@@ -16,7 +17,7 @@ def calculate(len1,len2):
     try:
         ans = len1 *100 / (len1 + len2)
     except:
-        ans = {"error": "0","model":"ไม่สามารถหาข้อมูลได้ข้อมูลที่ถูกใช้ต้องมีภาษาไทย"}
+        ans = "error"
 
     return ans
 
@@ -110,7 +111,24 @@ def repeat_word(list):  #เช็คคำซ้ำ
 def hightlihtforapi(text1,text2):
     txtlist =[]
     i = 0
-    list = word_tokenize(text2, engine="newmm", keep_whitespace=True)
+    ######ทดสอบ
+    import requests
+ 
+    url = "https://api.aiforthai.in.th/tlexplus"
+    
+    params = {'text':text2}
+    
+    headers = {
+        'Apikey': "4VSTV0w7wZEQTY1tV1v126v0gD1BR09W",
+        }
+    
+    response = requests.post(url, params=params, headers=headers)
+    
+    text = response.json()
+    list =(text['tokens'])
+    ######ทดสอบ
+    # list = word_tokenize(text2, engine="newmm", keep_whitespace=True)
+    # print(type(list))
     # list = text2.split()keep_whitespace=False
     # list = text2
     ori_text=""
@@ -126,7 +144,7 @@ def hightlihtforapi(text1,text2):
             i += 1
             if i >= len(list) :
                 break 
-            txt +=  " " + list[i].strip()
+            txt += " " + list[i].strip()
         #print(txt)
         if i < len(list) :
             txt = list[i].strip() 
